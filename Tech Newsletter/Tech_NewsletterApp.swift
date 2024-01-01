e777ffb75ccd198fab936d40091b430eaa5378ba
Rayyan <rayyanahmed101@yahoo.com>
2024-01-01 20:27:06 +0300

    Initial Commit

---
+++ Tech Newsletter/Tech_NewsletterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_NewsletterApp.swift
+//  Tech Newsletter
+//
+//  Created by MAC on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tech_NewsletterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
